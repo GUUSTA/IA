cbe65e49fc6c65f2f8ba7183b145d5272c793612
Gustavo Garcia Leite <gustavo031199@gmail.com>
2020-08-30 19:00:45 -0300

    Cria arquivo ep-1.py

---
+++ ep-1.py
@@ -0,0 +1,16 @@
+class ep1:
+
+def movimentoSucessor():
+    return
+def testeSucesso():
+    return
+def heuristica():
+    return
+def custo():
+    return
+def aEstrela():
+    return
+
+# Now you can call printme function
+printme("I'm first call to user defined function!")
+printme("Again second call to the same function")
