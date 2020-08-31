5a163502a8073d60046d13e2956f06dd20d37d58
Gustavo Garcia Leite <gustavo031199@gmail.com>
2020-08-31 00:09:51 -0300

    Arruma testeSucesso e custo

--- ep-1.py
+++ ep-1.py
@@ -6,42 +6,47 @@ class ep1:
 
     map: Map
     estadoAtual: Ponto
+    estadoFinal: Ponto
 
     def __init__(self, rangeX, rangeY):
         self.map = Map(rangeX, rangeY)
         self.estadoAtual = Ponto(rangeX - 1, 0, Terreno.INICIO)
+        self.estadoFinal = Ponto(0, rangeY - 1, Terreno.FINAL)
         
-    def movimentoSucessor(self):
+    def gerarSucessores(self):
         return
     
     def testeSucesso(self):
-        return
+        return self.estadoAtual == self.estadoFinal
     
-    def heuristica(self):
-        return
+    def heuristica(self, p1, p2):
+        a = math.pow((p2.x - p1.x), 2) + math.pow((p2.y - p1.y), 2)
+        b = math.sqrt(a)
+        return b
     
-    def custo(self, ponto_destino: Ponto):
-        if ponto_destino == Terreno.TERRA:
-            return 1
-        elif ponto_destino == Terreno.AGUA:
-            return 3
-        elif ponto_destino == Terreno.AREIA:
-            return 6
-        elif ponto_destino == Terreno.BARREIRA:
-            return 999
-        elif ponto_destino == Terreno.FINAL:
-            return -100
+    def custo(self, movimentos: [Ponto]):
+        custoTotal = 0
+        for movimento in movimentos:
+            if movimento == Terreno.TERRA:
+                custoTotal += 1
+            elif movimento == Terreno.AGUA:
+                custoTotal += 3
+            elif movimento == Terreno.AREIA:
+                custoTotal += 6
+            elif movimento == Terreno.BARREIRA:
+                custoTotal += 999
+            elif movimento == Terreno.FINAL:
+                custoTotal += 1
+            elif movimento == Terreno.INICIO:
+                custoTotal += 0
+        return custoTotal
+        
     
     def aEstrela(self):
         return
 
-    def distEuclidiana(self, p1, p2):
-        a = math.pow((p2.x - p1.x), 2) + math.pow((p2.y - p1.y), 2)
-        b = math.sqrt(a)
-        return b
 
 ep = ep1(6, 6)
-
 
 
 # p1 = Ponto(1, 2, Terreno.AGUA)
