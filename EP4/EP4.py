33d53c8d0035089f937ebcc13520723f0d208628
Eric <eric.zambom@outlook.com>
2020-11-06 20:16:26 -0300

    Melhora da visualização do resultado e final com porcentagem de acerttos e erros

--- EP4/EP4.py
+++ EP4/EP4.py
@@ -25,16 +25,16 @@ class EP4:
     negativeWords = {}
     totalValues = []
 
+    verifiedPositives = 0
+    verifiedNegatives = 0
+
     def __init__(self):
         self.createDataset()
         # self.train()
         self.loadModel()
-        for text in self.testablePositiveItems:
-            print()
-            print("Real resultado: Positivo")
-            self.test(text)
-            print()
-    
+        # self.classify()
+        self.test("O filme é um espetáculo visual! Fui assisti desprendido do original e deixei fluir, isso me proporcionou aproveitar e se surpreender com o novo Rei Leão. Claro que comparações são inevitáveis, ainda mais pra quem é fã do de 1994, mas esse tem algumas cenas diferentes e respeita bem a história do anterior. Filmaço!! Não tem como não se emocionar. A história por si só já é uma obra de arte. Nota 10!", True)
+
     def loadModel(self):
         self.positiveWords = readWordsFile("EP4/positives.csv")
         self.negativeWords = readWordsFile("EP4/negatives.csv")
@@ -42,10 +42,9 @@ class EP4:
         print(self.totalValues)
         # self.totalValues = []
 
-
     def formatText(self, array):
         normalized = [str.lower(item) for item in array]
-        splitted = [re.findall(r"[\w']+", item) for item in normalized]
+        splitted = [re.findall(r"[\w']+|[,.!?;]", item) for item in normalized]
         counted = [Counter(item) for item in splitted]
         summed = Counter({})
         for item in counted:
@@ -106,7 +105,8 @@ class EP4:
             totalNegatives += value
 
         print("Write Model")
-        totalValues = {totalPositives: len(positives), totalNegatives: len(negatives)}
+        totalValues = {totalPositives: len(
+            positives), totalNegatives: len(negatives)}
         writeWordValueFile(totalValues)
         writePositiveFile(positives)
         writeNegativeFile(negatives)
@@ -114,10 +114,10 @@ class EP4:
 
     def formatForTest(self, text):
         normalized = str.lower(text)
-        splitted = re.findall(r"[\w']+", normalized)
+        splitted = re.findall(r"[\w']+|[,.!?;]", normalized)
         return splitted
 
-    def test(self, text):
+    def test(self, text, positives):
         numberOfPositiveSentence = 24694
         numberOfNegativeSentence = 24765
         formattedText = self.formatForTest(text)
@@ -125,25 +125,86 @@ class EP4:
         first = True
         for word in formattedText:
             if first:
-                totalPositivo += math.log((numberOfPositiveSentence / (numberOfNegativeSentence + numberOfPositiveSentence)), 10)
+                totalPositivo += math.log((numberOfPositiveSentence / (
+                    numberOfNegativeSentence + numberOfPositiveSentence)), 10)
                 first = False
                 continue
-            wordCount = self.positiveWords[word] if word in self.positiveWords.keys() else 0
-            totalPositivo += math.log(((int(wordCount) + 1) / (int(self.totalValues['positiveTotalWords']) + int(self.totalValues['positiveWordsAmount']) + int(self.totalValues['negativeWordsAmount']))), 10)
-        print("%.10f" % totalPositivo)
+            wordCount = self.positiveWords[word] if word in self.positiveWords.keys(
+            ) else 0
+            totalPositivo += math.log(((int(wordCount) + 1) / (int(self.totalValues['positiveTotalWords']) + int(
+                self.totalValues['positiveWordsAmount']) + int(self.totalValues['negativeWordsAmount']))), 10)
+        # print("%.10f" % totalPositivo)
 
         totalNegativo = 0.0
         first = True
         for word in formattedText:
             if first:
-                totalNegativo += math.log((numberOfNegativeSentence / (numberOfNegativeSentence + numberOfPositiveSentence)), 10)
+                totalNegativo += math.log((numberOfNegativeSentence / (
+                    numberOfNegativeSentence + numberOfPositiveSentence)), 10)
                 first = False
                 continue
-            wordCount = self.negativeWords[word] if word in self.negativeWords.keys() else 0
-            totalNegativo += math.log(((int(wordCount) + 1) / (int(self.totalValues['negativeTotalWords']) + int(self.totalValues['negativeWordsAmount']) + int(self.totalValues['positiveWordsAmount']))), 10)
-        print("%.10f" % totalNegativo)
+            wordCount = self.negativeWords[word] if word in self.negativeWords.keys(
+            ) else 0
+            totalNegativo += math.log(((int(wordCount) + 1) / (int(self.totalValues['negativeTotalWords']) + int(
+                self.totalValues['negativeWordsAmount']) + int(self.totalValues['positiveWordsAmount']))), 10)
+        # print("%.10f" % totalNegativo)
 
-        print("Resultado: %s" % ("positivo" if totalPositivo > totalNegativo else "negativo"))
+        if totalPositivo > totalNegativo and positives:
+            self.verifiedPositives += 1
+        elif totalNegativo > totalPositivo and positives:
+            self.verifiedNegatives += 1
+        elif totalPositivo > totalNegativo and not positives:
+            self.verifiedPositives += 1
+        elif totalNegativo > totalPositivo and not positives:
+            self.verifiedNegatives += 1
+
+        print()
+        if positives:
+            temp = len(self.testablePositiveItems)
+            print("{} out of {} positives".format(self.verifiedPositives,
+                                                  temp))
+            print("{} negatives out of {} positives".format(
+                self.verifiedNegatives, temp))
+        else:
+            temp = len(self.testableNegativeItems)
+            print("{} out of {} negatives".format(self.verifiedNegatives,
+                                                  temp))
+            print("{} positives out of {} negatives".format(
+                self.verifiedPositives, temp))
+        print("Resultado: %s" %
+              ("positivo" if totalPositivo > totalNegativo else "negativo"))
+
+    def classify(self):
+        for text in self.testablePositiveItems:
+            # print()
+            # print("Real resultado: Positivo")
+            self.test(text, True)
+            # print()
+
+        percentage = self.verifiedPositives/len(self.testablePositiveItems)
+
+        self.verifiedPositives = 0
+        self.verifiedNegatives = 0
+
+        for text in self.testableNegativeItems:
+
+            # print("Real resultado: Negative")
+            self.test(text, False)
+            # print()
+
+        percentageN = self.verifiedNegatives/len(self.testableNegativeItems)
+
+        print()
+        print("FRASES POSITIVAS")
+        print("Porcentagem de Positivos: {:.2f}%".format(percentage*100))
+        print("Porcentagem de Falsos Negativos: {:.2f}%".format(
+            (1 - percentage)*100))
+
+        print()
+        print("FRASES NEGATIVAS")
+        print("Porcentagem de Negativos: {:.2f}%".format(percentageN*100))
+        print("Porcentagem de Falsos Positivos: {:.2f}%".format(
+            (1 - percentageN)*100))
 
 
 ep4 = EP4()
